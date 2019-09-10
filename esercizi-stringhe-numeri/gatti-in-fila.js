20668aafe0e51c1809dcc36678a098520643d1d6
zannaf <55135178+zannaf@users.noreply.github.com>
2019-09-10 11:44:06 +0200

    Update and rename esercizi-stringhe-numeri/gatti-in-fila.js to  esercizi-stringhe-numeri/gatti-in-fila.js

--- esercizi-stringhe-numeri/gatti-in-fila.js
+++ esercizi-stringhe-numeri/gatti-in-fila.js
@@ -14,6 +14,6 @@
   Consigli:
   Per l'arrotondamento ti consiglio di non usare Math.round (arrotonda all'intero superiore solo da .5 in su),
   utilizza Math.ceil (arrotonda sempre all'intero superiore).
-
-  http://www.imparareaprogrammare.it
+PER QUALCHE MOTIVO CHE IGNORO, SE SCRIVO GATTI IN INGLESE ME LO TRADUCE IN ITALIANO, OPPURE MI SCAMBIA DI POSTO LA T CON LA A (ANCHE LA ESSE CON LA A, HO FATTO ANCHE QUESTA PROVA)...
+http://www.imparareaprogrammare.it
 */
